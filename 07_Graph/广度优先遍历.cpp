8d7e461bd0c963175f3d3142f1b94450a30aad9d
Aurora <146014849+Aurora-73@users.noreply.github.com>
2023-10-03 23:42:16 +0800

    Add files via upload

---
+++ 07_Graph/广度优先遍历.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <cstring>
+#include <queue>
+using namespace std;
+
+const int MaxSize = 100;
+
+// 邻接矩阵表示的图结构体
+typedef struct MGraph {
+    char Vex[MaxSize];
+    int Edge[MaxSize][MaxSize];
+    int vexnum, arcnum;
+} MGraph;
+
+// 队列结构体
+typedef struct Queue {
+    int data[MaxSize];
+    int front, rear;
+} Queue;
+
+// 初始化队列
+void InitQueue(Queue &Q) {
+    Q.front = Q.rear = 0;
+}
+
+// 判断队列是否为空
+bool IsEmpty(Queue &Q) {
+    return Q.front == Q.rear;
+}
+
+// 入队列
+void EnQueue(Queue &Q, int x) {
+    Q.data[Q.rear++] = x;
+}
+
+// 出队列
+void DeQueue(Queue &Q, int &x) {
+    x = Q.data[Q.front++];
+}
+
+// 创建图的函数
+void CreateGraph(MGraph &G) {
+    cout << "请输入顶点信息（字符序列）: ";
+    cin >> G.Vex;
+    G.vexnum = strlen(G.Vex);
+
+    cout << "请逐行输入邻接矩阵，每行只包含0和1，无空格：" << endl;
+    for (int i = 0; i < G.vexnum; i++) {
+        string row;
+        cin >> row;
+        for (int j = 0; j < G.vexnum; j++) {
+            G.Edge[i][j] = row[j] - '0';
+        }
+    }
+}
+
+// 广度优先遍历
+void BFS(MGraph G, int v, int visited[]) {
+    Queue Q;
+    InitQueue(Q);
+    cout << G.Vex[v] << " ";
+    visited[v] = 1;
+    EnQueue(Q, v);
+    while (!IsEmpty(Q)) {
+        DeQueue(Q, v);
+        for (int i = 0; i < G.vexnum; i++) {
+            if (!visited[i] && G.Edge[v][i]) {
+            cout << G.Vex[i] << " ";
+                visited[i] = 1;
+                EnQueue(Q, i);
+            }
+        }
+    }
+    cout << endl;
+}
+
+int main() {
+    MGraph G;
+    CreateGraph(G);
+
+    int visited[MaxSize] = {0};
+    cout << "从哪个顶点开始广度优先遍历（输入对应的下标）: ";
+    int start;
+    cin >> start;
+    cout << "BFS遍历顺序: ";
+    BFS(G, start, visited);
+
+    return 0;
+}
